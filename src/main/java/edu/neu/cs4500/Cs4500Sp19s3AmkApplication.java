29ebde0dfb3e400af10c7eb7aaaadcf512b6ca3f
amkotowski <alice@Alices-MacBook-Pro.local>
2019-01-16 21:12:49 -0500

    refactoring name

--- src/main/java/edu/neu/cs4500/Cs4500Sp19s3AmkApplication.java
+++ src/main/java/edu/neu/cs4500/Cs4500Sp19s3AmkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cs4500Sp19s1AmkApplication {
+public class Cs4500Sp19s3AmkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cs4500Sp19s1AmkApplication.class, args);
+		SpringApplication.run(Cs4500Sp19s3AmkApplication.class, args);
 	}
 
 }
